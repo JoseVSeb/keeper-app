0a617d8fcacbb83924cc4cc97e94890277504a30
JoseVSeb <josevseb99@gmail.com>
2021-10-05 23:05:01 +0530

    Add React components as described in the challenge
    1. Create a new React app.
    2. Create a App.jsx component.
    3. Create a Header.jsx component that renders a <header> element
    to show the Keeper App name in an <h1>.
    4. Create a Footer.jsx component that renders a <footer> element
    to show a copyright message in a <p> with a dynamically updated year.
    5. Create a Note.jsx component to show a <div> element with a
    <h1> for a title and a <p> for the content.

---
+++ src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+    return (
+        <div>
+            <h1>Title</h1>
+            <p>This is the content of the note element.</p>
+        </div>
+    );
+}
+
+export default Note;
